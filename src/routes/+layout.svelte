0b350ff48d884f294c37121005dfc9bcd74c9029
tora29 <nikumann.power@gmail.com>
2024-09-09 01:05:27 +0900

    共通レイアウトで背景を取り込み

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang=ts>
   import BaseHeader from "$lib/components/BaseHeader.svelte";
   import BaseFooter from "$lib/components/BaseFooter.svelte";
+  import BaseBackground from "$lib/components/BaseBackground.svelte";
 </script>
 
+<BaseBackground />
 <BaseHeader />
 <div class="pt-40">
   <slot />

38ec14a9a82ea6eda0c7fdfab85e03bd4ff25866
tora29 <nikumann.power@gmail.com>
2024-09-08 21:41:42 +0900

    フッターを追加

---
+++ src/lib/components/BaseFooter.svelte
@@ -0,0 +1,9 @@
+<script lang=ts>
+  
+</script>
+
+<footer class="footer footer-center bg-base-300 text-base-content p-4 absolute bottom-0">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Tora29 Industries Ltd</p>
+  </aside>
+</footer>

2af7ad2bafa5c58794fdd6d006cb4933a4fbd1a9
tora29 <nikumann.power@gmail.com>
2024-09-08 21:42:25 +0900

    ヘッダーとフッターをインポート

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang=ts>
+  import BaseHeader from "$lib/components/BaseHeader.svelte";
+  import BaseFooter from "$lib/components/BaseFooter.svelte";
+</script>
+
+<BaseHeader />
+<div class="pt-40">
+  <slot />
+</div>
+<BaseFooter />

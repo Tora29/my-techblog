67b84ee6917ac8bf69676d9b20afb572c111f936
tora29 <nikumann.power@gmail.com>
2024-09-10 23:00:37 +0900

    カード内容の定数追加

---
+++ src/lib/data/ConstCards.ts
@@ -0,0 +1,6 @@
+export const constCards = [
+    { imgSrc: '/myImage.jpg', imgAlt: 'MyImage', title: 'About Me', description: '私です。' },
+    { imgSrc: '/workImage.jpg', imgAlt: 'WorkImage', title: 'Work', description: '私の作品' },
+    { imgSrc: '/techImage.jpg', imgAlt: 'TechImage', title: 'My Tech Blog', description: '技術ブログ' },
+    { imgSrc: '/contactImage.jpg', imgAlt: 'ContactImage', title: 'Contact', description: 'コンタクト' },
+  ];

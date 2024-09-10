3f3b554cb6a20a988986ed30617dc19cd238b774
tora29 <nikumann.power@gmail.com>
2024-09-10 23:01:49 +0900

    カードの表示ロジックを追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import BaseCard from "$lib/components/BaseCard.svelte";
+  import { constCards } from "$lib/data/ConstCards";
+</script>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center pt-32">
+  {#each constCards as card}
+    <BaseCard 
+      {...card}
+    />
+  {/each}
+</div>

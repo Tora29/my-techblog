07f2dd2d974df5eb44c87426fb599935e3367390
tora29 <nikumann.power@gmail.com>
2024-09-25 00:43:17 +0900

    必要ないファイルを削除

--- src/lib/data/ConstCards.ts
+++
@@ -1,6 +0,0 @@
-export const constCards = [
-    { imgSrc: '/myImage.jpg', imgAlt: 'MyImage', title: 'About Me', description: '私です。' },
-    { imgSrc: '/workImage.jpg', imgAlt: 'WorkImage', title: 'Work', description: '私の作品' },
-    { imgSrc: '/techImage.jpg', imgAlt: 'TechImage', title: 'My Tech Blog', description: '技術ブログ' },
-    { imgSrc: '/contactImage.jpg', imgAlt: 'ContactImage', title: 'Contact', description: 'コンタクト' },
-  ];

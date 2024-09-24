d4c945e770a0f716d4f9a8f39184ce05157b6ba3
tora29 <nikumann.power@gmail.com>
2024-09-24 20:48:30 +0900

    カードコンポーネントの定義を変更

--- src/lib/components/BaseCard.svelte
+++ src/lib/components/BaseCard.svelte
@@ -1,34 +1,25 @@
 <script lang="ts">
-  export let imgSrc: string;      // 画像のsrc
-  export let imgAlt: string;      // 画像のalt
-  export let title: string;       // カードのタイトル
-  export let description: string; // カードの説明文
+	// コンポーネントの変数
+	export let link: string = '/';
+	export let classType: string = 'card';
+	export let cardType: string = 'card-side';
+	export let color: string = 'bg-primary';
+	export let height: string = 'h-40';
+	export let width: string = 'w-100';
+	export let imgSrc: string = '';
+	export let imgAlt: string = '';
+	export let imgHeight: string = 'h-40';
+	export let imgWidth: string = 'w-72';
+	export let cardTitle: string = 'card-title';
+	export let cardText: string = 'card-text';
 </script>
 
-<div class="card bg-base-100 image-full w-96 transform transition-transform duration-300 hover:scale-105">
-  <figure>
-    <img src={imgSrc} alt={imgAlt} />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">{title}</h2>
-    <p>{description}</p>
-  </div>
-</div>
-
-<style>
-  .card-body h2,.card-body p {
-  color: white; 
-  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.8); 
-}
-
-  /* カードのスタイル */
-  .card {
-    border: 2px solid rgba(255, 255, 255, 0.2);
-    transition: border-color 0.3s ease, transform 0.3s ease;
-  }
-
-  /* カードがホバーされたときのエフェクト */
-  .card:hover {
-    border-color: rgba(255, 255, 255, 0.5);
-  }
-</style>
+<a href={link} class="{classType} {cardType} {color} {height} {width}">
+	<figure>
+		<img src={imgSrc} alt={imgAlt} class="{imgHeight} {imgWidth}" />
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">{cardTitle}</h2>
+		<p>{cardText}</p>
+	</div>
+</a>

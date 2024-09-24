a5825de94a2fdf6a05b004e89c82951493577366
tora29 <nikumann.power@gmail.com>
2024-09-24 20:49:09 +0900

    フッターの定義を変更

--- src/lib/components/BaseFooter.svelte
+++ src/lib/components/BaseFooter.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
-  
+	// コンポーネントの変数
+	export let footer: string = 'footer';
+	export let footerPosition: string = 'footer-center';
+	export let color: string = 'bg-primary';
+	export let padding: string = 'p-4';
 </script>
 
-<footer class="footer footer-center bg-base-300 text-base-content p-4 absolute bottom-0">
-  <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by Tora29 Industries Ltd</p>
-  </aside>
+<footer class="{footer} {footerPosition} {color} {padding}">
+	<aside>
+		<p>Copyright © {new Date().getFullYear()} - All right reserved by Tora29 Industries Ltd</p>
+	</aside>
 </footer>
+
+<style>
+	.footer {
+		bottom: 0;
+	}
+</style>

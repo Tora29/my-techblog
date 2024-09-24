bbaf4a68b003b9359f1496b6ce541f601ea9d338
tora29 <nikumann.power@gmail.com>
2024-09-25 00:41:29 +0900

    myTheme を追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      }
-    },
-  },
-  plugins: [require('daisyui')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	daisyui: {
+		themes: [
+			{
+				myLightTheme: {
+					primary: '#DBD0C0', // ライトベージュ
+					secondary: '#DCBA92', // ゴールドベージュ
+					accent: '#1D1D1D', // ダークグレー
+					neutral: '#1D1D1D',
+					'base-100': '#FFFFFF' // ベースカラーホワイト
+				},
+				myDarkTheme: {
+					primary: '#141424', // ネイビー系
+					secondary: '#B5B5B6', // 明るいグレー
+					accent: '#323240', // 暗いグレー系
+					neutral: '#141424', // 同じネイビー系を使用
+					'base-100': '#000000' // ベースカラーはブラック
+				}
+			}
+		]
+	},
+	theme: {
+		extend: {
+			screens: {
+				sm: '640px',
+				md: '768px',
+				lg: '1024px',
+				xl: '1280px'
+			}
+		}
+	},
+	plugins: [require('daisyui')]
 };

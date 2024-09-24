0295443b2230aabc3c07c68ba9a29c5b040b858c
tora29 <nikumann.power@gmail.com>
2024-09-25 01:22:33 +0900

    上下のpadding 幅を調整

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,9 @@
 	export let cardItems = constCardItems;
 </script>
 
-<div class="grid grid-cols-12 gap-4 pt-40 pb-40">
+<div class="grid grid-cols-12 gap-4 pb-20">
 	{#each cardItems as cardItem}
-		<div class="col-span-12 md:col-start-3 md:col-span-8">
+		<div class="col-span-12 md:col-start-3 md:col-span-8 pt-20">
 			<BaseCard
 				link={cardItem.link}
 				imgSrc={cardItem.imgSrc}

522bc6de9ef619bac59c0d9d5d530cc90014834b
tora29 <nikumann.power@gmail.com>
2024-09-25 01:19:06 +0900

    共通背景を削除

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,8 @@
-<script lang=ts>
-  import BaseHeader from "$lib/components/BaseHeader.svelte";
-  import BaseFooter from "$lib/components/BaseFooter.svelte";
-  import BaseBackground from "$lib/components/BaseBackground.svelte";
+<script lang="ts">
+	import BaseHeader from '$lib/components/BaseHeader.svelte';
+	import BaseFooter from '$lib/components/BaseFooter.svelte';
 </script>
 
-<BaseBackground />
 <BaseHeader />
-<div class="pt-40">
-  <slot />
-</div>
+<slot />
 <BaseFooter />

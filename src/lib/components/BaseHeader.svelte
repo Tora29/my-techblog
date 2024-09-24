eb5f1e4a999b78fbd16fad72c7789cdef6b3650c
tora29 <nikumann.power@gmail.com>
2024-09-25 01:17:22 +0900

    テーマ変更コンポーネントを追加、ヘッダーでインポート

--- src/lib/components/BaseHeader.svelte
+++ src/lib/components/BaseHeader.svelte
@@ -3,30 +3,33 @@
 	export let navbar: string = 'navbar';
 	export let color: string = 'bg-primary';
 
-  // CommonStyle
+	// CommonStyle
 	const commonStyle: string = 'join-item w-40';
 
 	// CommonJoin に値を格納する
 	export let joinItems = [
 		{ class: commonStyle, text: 'ABOUT ME', link: '/about' },
-		{ class: commonStyle, text: 'SERVICE', link: '/service' },
 		{ class: commonStyle, text: 'BLOG', link: '/blog' },
-		{ class: commonStyle, text: 'CONTACT', link: '/contact' }
+		{ class: commonStyle, text: 'SERVICE', link: '/service' }
 	];
 
 	// コンポーネントのインポート
 	import CommonJoin from './CommonJoin.svelte';
 	import CommonButton from './CommonButton.svelte';
+	import BaseThemeController from '$lib/components/BaseThemeController.svelte';
 </script>
 
 <!-- Header -->
 <div class={`${navbar} ${color} relative flex items-center h-40`}>
 	<!-- トップ -->
-<div class="absolute top-0 left-0 m-4 md:m-6">
-    <CommonButton text="Tora29" {commonStyle} />
-</div>
+	<div class="absolute top-0 left-0 m-4 md:m-6">
+		<CommonButton text="Tora29" {commonStyle} />
+	</div>
 	<div class="flex-1 flex justify-center">
 		<CommonJoin {joinItems} />
+	</div>
+	<div>
+		<BaseThemeController />
 	</div>
 </div>
 

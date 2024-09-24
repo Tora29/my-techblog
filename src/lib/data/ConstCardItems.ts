2c6c2f346c47fcb4f86d9ffcb8107fdc8519cc46
tora29 <nikumann.power@gmail.com>
2024-09-24 20:50:47 +0900

    カード内容を定義する定数ファイルを追加

---
+++ src/lib/data/ConstCardItems.ts
@@ -0,0 +1,29 @@
+export const constCards = [
+	{
+		link: '/about',
+		imgSrc: 'myImage.jpg',
+		imgAlt: 'MyImage',
+		cardTitle: 'ABOUT ME',
+		cardText: '私です。',
+		height: 'h-80',
+		imgHeight: 'h-80'
+	},
+	{
+		link: '/about',
+		imgSrc: 'techImage.jpg',
+		imgAlt: 'techImage',
+		cardTitle: 'TECH BLOG',
+		cardText: '私です。',
+		height: 'h-80',
+		imgHeight: 'h-80'
+	},
+	{
+		link: '/service',
+		imgSrc: 'workImage.jpg',
+		imgAlt: 'workImage',
+		cardTitle: 'SERVICE',
+		cardText: '私です。',
+		height: 'h-80',
+		imgHeight: 'h-80'
+	}
+];

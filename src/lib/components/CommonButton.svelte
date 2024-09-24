c2a96efa4a41bae16d902d46614e11a377ab9293
tora29 <nikumann.power@gmail.com>
2024-09-24 20:50:21 +0900

    ボタンコンポーネントと連結ボタンのコンポーネントを追加

---
+++ src/lib/components/CommonButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	// コンポーネントの変数
+	export let link: string = '/';
+	export let classType: string = 'btn';
+	export let text: string = 'Button';
+	export let style: string = '';
+	export let commonStyle: string = '';
+</script>
+
+<a href={link} class={`${classType} ${style} ${commonStyle}`}>{text}</a>
---
+++ src/lib/components/CommonJoin.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	// コンポーネントの変数
+	export let type: string = 'join';
+	export let joinItems: { link: string; class: string; text: string }[];
+
+	// コンポーネントのインポート
+	import CommonButton from './CommonButton.svelte';
+</script>
+
+<div class={type}>
+	{#each joinItems as item}
+		<CommonButton link={item.link} style={item.class} text={item.text} />
+	{/each}
+</div>

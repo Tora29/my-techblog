07ce291c6e0cf029bb6acf2babe2b697a9cf4e2f
tora29 <nikumann.power@gmail.com>
2024-10-01 19:48:09 +0900

    vitest とその設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
+/// <reference types="vitest" />
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.vitest.ts']
 	}
 });

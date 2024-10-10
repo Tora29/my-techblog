d83a183d1f0d565db54daac53725ee361f6e2301
tora29 <nikumann.power@gmail.com>
2024-10-10 21:13:42 +0900

    コンポーネントのクラスを最適化

--- src/lib/components/CommonJoin.svelte
+++ src/lib/components/CommonJoin.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 	// コンポーネントの変数
-	export let type: string = 'join';
 	export let joinItems: { link: string; class: string; text: string }[];
 
 	// コンポーネントのインポート
 	import CommonButton from './CommonButton.svelte';
 </script>
 
-<div class={type}>
+<div class="join">
 	{#each joinItems as item}
-		<CommonButton link={item.link} style={item.class} text={item.text} />
+		<CommonButton link={item.link} classType={item.class} text={item.text} />
 	{/each}
 </div>

0646c0a1b8eb4460b965ef51c1b804090563d0f5
tora29 <nikumann.power@gmail.com>
2024-10-10 21:15:26 +0900

    テキストインプットコンポーネントを追加

---
+++ src/lib/components/BaseTextInput.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	// コンポーネントの変数
+	export let placeholder: string = 'Input here';
+	export let style: string = 'input-bordered';
+	export let color: string = 'input-primary';
+	export let size: string = 'input-md';
+</script>
+
+<input type="input" {placeholder} class="input {style} {color} {size}" />

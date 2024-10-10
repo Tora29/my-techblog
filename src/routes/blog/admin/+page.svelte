15c523d8eed44fd58defcf948852fb85e2de98a9
tora29 <nikumann.power@gmail.com>
2024-10-10 21:14:16 +0900

    管理者用記事投稿ページを追加

---
+++ src/routes/blog/admin/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import BaseTextInput from '$lib/components/BaseTextInput.svelte';
+	import BaseMarkdownEditor from '$lib/components/BaseMarkdownEditor.svelte';
+	import CommonButton from '$lib/components/CommonButton.svelte';
+</script>
+
+<div class="p-4">
+	<BaseTextInput placeholder="記事タイトル" />
+</div>
+
+<BaseMarkdownEditor />
+
+<div class="p-4">
+	<CommonButton text="Submit" />
+</div>

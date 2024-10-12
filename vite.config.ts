2c19ed77980fef401d9d053813edbb26e04b89b6
tora29 <nikumann.power@gmail.com>
2024-10-12 21:23:26 +0900

    ファイル名の変更とicnlude 先の変更

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'jsdom',
-		include: ['src/**/*.vitest.ts']
+		include: ['src/**/*.test.ts'],
 	},
 	optimizeDeps: {
 		exclude: ['dompurify']

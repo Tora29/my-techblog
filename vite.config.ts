3c71658815ea3f3e78b6493e764d0fbfaddac09a
tora29 <nikumann.power@gmail.com>
2024-10-19 05:40:42 +0900

    feature/supabase->firebase, remove-dotenv, add-sample-page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 /// <reference types="vitest" />
 export default defineConfig({
